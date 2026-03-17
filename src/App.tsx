ed468c7a8fdcf8b1b976b33978c338c490d50e68
pedror0604 <pee100.pr22@gmail.com>
2026-03-17 12:03:06 -0300

    tamanho bebida

--- src/App.tsx
+++ src/App.tsx
@@ -412,7 +412,7 @@ function HomeView({ addToCart, goToCustom }) {
               <img
                 src={item.img}
                 alt={item.name}
-                className="w-16 h-16 rounded-lg object-cover"
+                className={`w-16 h-16 rounded-lg object-contain ${item.name.includes("Zero") ? "scale-125" : ""}`}
               />
               <div className="flex-1">
                 <h4 className="font-bold text-gray-800 text-sm">{item.name}</h4>
